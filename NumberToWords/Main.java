31ab9d458adb7e122c0aa321f2f41161dc80a680
Ratheshan03 <lionratheshan@gmail.com>
2021-09-30 12:31:07 +0530

    update-rename

--- NumberToWords/Main.java
+++ NumberToWords/Main.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class Mains {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number: ");
